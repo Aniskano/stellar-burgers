eebfee1dd3b7bc351f2aa74c9ee9ca005b15e429
Anna Novikova <anyano@mail.ru>
2025-02-24 20:03:31 +0300

    build: update libs, vite

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  base: '/stellar-burgers/',
+  plugins: [react(), tsconfigPaths(), legacy()],
+  envPrefix: ['VITE_', 'BURGER_']
+});
